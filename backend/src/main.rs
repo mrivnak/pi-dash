ac6c9be077d8982558d07c3ab415c13003850aa6
Michael Rivnak <rivnakm1@gmail.com>
2022-03-08 16:19:15 -0500

    basic dist rendering

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use pretty_env_logger;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    warp::serve(warp::fs::dir("../frontend/dist"))
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
