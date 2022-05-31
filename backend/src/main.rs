c58e7bc48f9a57e833fc9804fca865cec796f5cf
Michael Rivnak <rivnakm1@gmail.com>
2022-05-30 21:59:11 -0400

    Switch to axum and graphql

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,3 @@
-use pretty_env_logger;
-
-#[tokio::main]
-async fn main() {
-    pretty_env_logger::init();
-
-    warp::serve(warp::fs::dir("../frontend/dist"))
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+fn main() {
+    println!("Hello, world!");
 }
